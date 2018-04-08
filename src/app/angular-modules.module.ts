7ad82adb649d405d9975202d1c62c14cd036db62
Miguel <a@b.c>
2018-04-08 03:01:07 -0300

    formatação editar tarefa

--- src/app/angular-modules.module.ts
+++ src/app/angular-modules.module.ts
@@ -11,16 +11,21 @@ import {MatDividerModule} from '@angular/material/divider';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 
+import {LayoutModule} from '@angular/cdk/layout';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
 @NgModule({
   imports: [
     MatButtonModule, MatCheckboxModule, MatTabsModule, MatToolbarModule,
     MatCardModule, MatFormFieldModule, MatSnackBarModule, MatChipsModule,
-    MatDividerModule, MatIconModule, MatInputModule,
+    MatDividerModule, MatIconModule, MatInputModule, FlexLayoutModule,
+    LayoutModule,
   ],
   exports: [
     MatButtonModule, MatCheckboxModule, MatTabsModule, MatToolbarModule,
     MatCardModule, MatFormFieldModule, MatSnackBarModule, MatChipsModule,
-    MatDividerModule, MatIconModule, MatInputModule,
+    MatDividerModule, MatIconModule, MatInputModule, FlexLayoutModule,
+    LayoutModule,
   ]
 })
 export class AngularModulesModule { }
